bca6492f4f0306840b401ac97079c54e186654b8
Peter Law <PeterJCLaw@gmail.com>
2015-10-25 21:55:13 +0000

    Extract user loading to a helper
    This will allow us to load Colleges without loading all their users
    in cases where we just want the name of the college.
    
    Change-Id: I483f11f461e1820725bde580108105178612206a

--- nemesis/static/js/college.js
+++ nemesis/static/js/college.js
@@ -8,9 +8,8 @@ var College = function() {
         this.users = [];
         this.teams = [];
 
-        this.fetch = function(callback) {
-            wv.start("Loading users");
-            $.get("colleges/" + this.canonical_name, function(response) {
+        var fetch_helper = function(callback) {
+            $.get("colleges/" + that.canonical_name, function(response) {
                 if (typeof(response) == "string") {
                     response = JSON.parse(response);
                 }
@@ -18,19 +17,31 @@ var College = function() {
                 that.english_name = response.name;
                 that.teams = response.teams;
                 var user_requests = response.users.length;
-                wv.start("Fetching users");
-                that.users = $.map(response.users, function(v) {
-                    u = new User(v);
-                    u.fetch(function() {
-                        user_requests--;
-                        if (user_requests == 0) {
-                            Colleges[that.canonical_name] = that;
-                            callback(that);
-                            wv.hide();
-                        }
-                    });
+                that.users = $.map(response.users, function(v) { return new User(v); });
+                Colleges[that.canonical_name] = that;
+                callback(that);
+            });
+        };
 
-                    return u;
+        this.fetch = function(callback) {
+            wv.start("Loading colleges");
+            fetch_helper(function() {
+                wv.start("Fetching users");
+                that.load_users(function(college) {
+                    wv.hide();
+                    callback(college);
+                });
+            });
+        };
+
+        this.load_users = function(callback) {
+            var user_requests = that.users.length;
+            $.map(that.users, function(u) {
+                u.fetch(function() {
+                    user_requests--;
+                    if (user_requests == 0) {
+                        callback(that);
+                    }
                 });
             });
         };
