ff392ac29484fa78121e26b695e2b74fe8f3ceb4
Peter Law <PeterJCLaw@gmail.com>
2015-10-28 23:51:18 +0000

    Whitespace & code tidyup
    Change-Id: I734c1bc856af2e7eb8c486462a37b4c7934f15f9

--- nemesis/static/js/auth_view.js
+++ nemesis/static/js/auth_view.js
@@ -4,14 +4,15 @@ var AuthView = function() {
 
         this.display_auth_error = function(auth_errors_list) {
             node.text(human_readable_error(auth_errors_list));
-        }
+        };
 
         var human_readable_error = function(auth_errors_list) {
             var reasons = {
-                       "WRONG_PASSWORD":"Password incorrect"}
+                "WRONG_PASSWORD": "Password incorrect"
+            };
 
             var result = "";
-            for (key in reasons) {
+            for (var key in reasons) {
                 if (reasons.hasOwnProperty(key) && auth_errors_list.indexOf(key) != -1) {
                     result += reasons[key];
                 }
@@ -19,5 +20,5 @@ var AuthView = function() {
 
             return result;
         };
-    }
+    };
 }();
