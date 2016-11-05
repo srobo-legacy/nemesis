d7ef4cba788bbebedddc549f8726d7a2fb3ab87e
Peter Law <PeterJCLaw@gmail.com>
2016-11-05 19:33:08 +0000

    Extract helper for rendering colleges
    Change-Id: Ic606838fb141b6d7c27971bb2a964498e54f204b

--- nemesis/static/js/college_list_view.js
+++ nemesis/static/js/college_list_view.js
@@ -11,6 +11,38 @@ var CollegeListView = function() {
         var media_consent_template = TemplateExpander.template('media_consent');
         var register_template = TemplateExpander.template("register_link");
 
+        var expand_college_template = function(college, allow_registration) {
+            $(college.users).each(function(idx, u) {
+                var args = {
+                    'first': u.first_name,
+                    'last': u.last_name
+                };
+                if (u.has_media_consent) {
+                    args.not = '';
+                    args.icon = 'camera';
+                } else {
+                    args.not = 'not ';
+                    args.icon = 'none';
+                }
+                u.media = media_consent_template.render_with(args);
+            });
+            $.each(college.users, function (index, user) {
+                user.class = user.has_withdrawn ? "disabled" : undefined;
+            });
+            var user_templates = user_template.map_over("user", college.users);
+            var register_link = '';
+            if (allow_registration) {
+                register_link = register_template.render_with({"college":college});
+            }
+            var final_render = college_template.render_with({
+                "users": user_templates,
+                "register": register_link,
+                "college": college
+            });
+
+            return final_render;
+        };
+
         this.render_colleges = function(college_list, allow_registration) {
             colleges = college_list;
             allow_registration_last = allow_registration;
@@ -18,35 +50,7 @@ var CollegeListView = function() {
             var result = "";
             for (var i = 0; i < college_list.length; i++) {
                 var college = college_list[i];
-                $(college.users).each(function(idx, u) {
-                    var args = {
-                        'first': u.first_name,
-                        'last': u.last_name
-                    };
-                    if (u.has_media_consent) {
-                        args.not = '';
-                        args.icon = 'camera';
-                    } else {
-                        args.not = 'not ';
-                        args.icon = 'none';
-                    }
-                    u.media = media_consent_template.render_with(args);
-                });
-                $.each(college.users, function (index, user) {
-                    user.class = user.has_withdrawn ? "disabled" : undefined;
-                });
-                var user_templates = user_template.map_over("user", college.users);
-                var register_link = '';
-                if (allow_registration) {
-                    register_link = register_template.render_with({"college":college});
-                }
-                var final_render = college_template.render_with({
-                    "users": user_templates,
-                    "register": register_link,
-                    "college": college
-                });
-
-                result += final_render;
+                result += expand_college_template(college, allow_registration);
             }
 
             node.html(result);
