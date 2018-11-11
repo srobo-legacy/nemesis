26f68c517c8d9182ce0245f937bdd765b08a14ff
Peter Law <PeterJCLaw@gmail.com>
2018-11-11 15:24:23 +0000

    Make email sending script configurable
    Change-Id: Ie962f71e97f826934807a3d4b4aa6e5fc4fe70ca

--- nemesis/scripts/send-emails.py
+++ nemesis/scripts/send-emails.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import os
 import sys
 
@@ -10,5 +11,25 @@ import config
 import helpers
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="""
+        Attempt to send pending emails. Each pending email (up to a limit) will
+        be considered once per invocation, up to ``--max-retries'' number of
+        times. Older emails will be considered before newer emails.
+        """,
+    )
+    parser.add_argument(
+        '--limit',
+        default=50,
+        help="The maximum number of emails to send (default: %(default)s).",
+    )
+    parser.add_argument(
+        '--max-retries',
+        default=5,
+        help="The maximum number of retries to allow before considering an email"
+             " abandoned (default: %(default)s).",
+    )
+    args = parser.parse_args()
+
     config.configure_logging()
-    helpers.send_emails()
+    helpers.send_emails(limit=args.limit, max_retry=args.max_retries)
